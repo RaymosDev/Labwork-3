2f7a409d113f89132840416229a90fb584823706
Roman_Mishchiryakov <raycastm22@gmail.com>
2022-05-22 20:29:52 +0300

    Добавьте файлы проекта.

---
+++ Labwork-3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace Labwork_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Centrobank();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private String Centrobank()
+        {
+            WebClient client = new WebClient();
+            String parsedHtmlPage = client.DownloadString("http://www.finmarket.ru/currency/USD/");
+            String parsedValue = System.Text.RegularExpressions.Regex.Match(parsedHtmlPage, @"<div class=""valvalue"">([0-9]+\,[0-9]+)</div>").Groups[1].Value;
+            return "Курс USD (ЦБ): " + parsedValue;
+
+        }
+    }
+}
